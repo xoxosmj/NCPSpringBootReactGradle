b73e15f5edc2a9711fa0b4c45a6f54347ae7ba10
semook <xoxosmj@gmail.com>
2024-11-15 14:35:21 +0900

    first commit

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Spring Boot + React + NCP</h1>
+            <h3>카운트 : 1</h3>
+            <img src={'./images/안경우사기와 책.jpeg'} alt={"공부하자"}/>
+
+        </div>
+    );
+};
+
+export default App;
---
+++ src/test/java/com/example/ncpspringbootreactgradle/NcpSpringBootReactGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncpspringbootreactgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcpSpringBootReactGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
